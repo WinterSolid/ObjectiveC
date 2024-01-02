8685866ffa8e62b55c9b57f8d31a6da102cb4d58
wintersolid <ztanksley@luc.edu>
2024-01-02 11:54:34 -0800

    Setting up Cells for table view

--- ObjectiveC/ViewController.m
+++ ObjectiveC/ViewController.m
@@ -18,6 +18,21 @@
     // Do any additional setup after loading the view.
     self.navigationItem.title = @"Courses";
     self.navigationController.navigationBar.prefersLargeTitles = YES;
+    //method call
+    [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier: @"cellId"];
+
+}
+
+//Number of rows in TableView
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView numberOfRowsInSection: (NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //Type    pointer Object
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellId" forIndexPath:indexPath];
+    cell.backgroundColor = UIColor.redColor;
+    return cell;
 }
 
 
